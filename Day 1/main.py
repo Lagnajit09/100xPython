862990a8b1958c6a7d97adcb114fdbeaec043470
Lm09 <123.moharana@gmail.com>
2024-07-08 14:45:14 +0530

    Day-1

---
+++ Day 1/main.py
@@ -0,0 +1,39 @@
+# Write your code below this line 👇
+print("Hello World")
+print("Hello World\nHJello World")
+
+print('Hello "World"')
+
+print("Hello \"World\"")
+
+print("Hello" + " " + "World")
+
+# //IndentationError
+# print("Hello" + " World")
+
+# INPUT FUNCTION
+input("What is your name?")
+
+print("Hello" + input("What is your name?") + "!")
+
+# len function
+numOfLetters = len("Angela")
+print(numOfLetters)
+
+# Find the length of any string
+print(len(input()))
+
+# Variables
+name = input("What is your name?")
+print(name)
+
+# Name error
+# user_name = input("What is your name?")
+# print(username)
+
+# Project 1: Band name generator
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+
