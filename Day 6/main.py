6cb326d71cd5cf01465f1d98951547a6f3aec5c0
Lm09 <123.moharana@gmail.com>
2024-07-13 18:25:13 +0530

    Day 6

---
+++ Day 6/main.py
@@ -0,0 +1,34 @@
+# Python functions
+
+# Defining a function
+def my_function():
+    print("Hello")
+    print('Bye')
+
+# Function call
+my_function()
+
+
+# Function with return
+def add(a, b):
+    return a + b
+
+result = add(3, 5)
+print(result)  # Output: 8
+
+
+# While loop
+# while condition:
+#     # code to execute
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+
+# Infinite loop
+# The following code will run infite times
+# while True:
+#     print("This is an infinite loop")
+
