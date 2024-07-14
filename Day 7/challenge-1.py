27a105f519ae4d5a73a28c66f6486f724f6365e2
Lm09 <123.moharana@gmail.com>
2024-07-14 21:57:26 +0530

    Day 7

---
+++ Day 7/challenge-1.py
@@ -0,0 +1,17 @@
+# Challenge - 1
+
+#Step 1
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input().lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+lives = 7
+if chosen_word.find(guess) == -1:
+  lives -= 1
