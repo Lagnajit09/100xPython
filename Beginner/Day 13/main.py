7fdf2dfe0b46214ddbd16b4f632880781d2b409b
Lm09 <123.moharana@gmail.com>
2024-07-24 23:24:33 +0530

    restructure

--- Beginner/Day 13/main.py
+++ Beginner/Day 13/main.py
@@ -1,6 +1,6 @@
 ############DEBUGGING#####################
 
-# # Describe Problem
+# # 1 Describe Problem
 # def my_function():
 #   # Bug: range() doesnt include upperbound
 #   # for i in range(1, 20):
@@ -9,7 +9,7 @@
 #       print("You got it")
 # my_function()
 
-# # Reproduce the Bug
+# # 2 Reproduce the Bug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
 # # Bug: the indices starts from 0 to len-1
@@ -17,7 +17,7 @@
 # dice_num = randint(0, 5)
 # print(dice_imgs[dice_num])
 
-# # Play Computer
+# # 3 Play Computer
 # year = int(input("What's your year of birth?"))
 # if year > 1980 and year < 1994:
 #   print("You are a millenial.")
@@ -26,7 +26,7 @@
 # elif year >= 1994:
 #   print("You are a Gen Z.")
 
-# # Fix the Errors
+# # 4 Fix the Errors
 # # int cant be compared with str
 # # age = input("How old are you?")
 # age = int(input("How old are you?"))
@@ -35,21 +35,20 @@
 #   # print("You can drive at age {age}.")
 #   print(f"You can drive at age {age}.")
 
-# #Print is Your Friend
+# # 5 Print is Your Friend
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
-
 # # == used for comparison while = used for assigment
 # # word_per_page == int(input("Number of words per page: "))
 # word_per_page = int(input("Number of words per page: "))
 
-# # debug: print statement
+# # 6 debug: print statement
 # print(pages, word_per_page)
 # total_words = pages * word_per_page
 # print(total_words)
 
-# #Use a Debugger
+# # 7 Use a Debugger
 # # https://pythontutor.com/python-compiler.html#mode=edit
 # def mutate(a_list):
 #   b_list = []
@@ -58,7 +57,5 @@
 #   # it should be indented to append all the items in the new list
 #   # b_list.append(new_item)
 #     b_list.append(new_item)
-    
 #   print(b_list)
-
 # mutate([1,2,3,5,8,13])
