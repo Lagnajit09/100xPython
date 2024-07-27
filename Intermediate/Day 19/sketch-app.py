0fff57033242b5fd70bc012650cd591ae1462fc7
Lm09 <123.moharana@gmail.com>
2024-07-27 23:15:43 +0530

    Day 19

---
+++ Intermediate/Day 19/sketch-app.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+
+# passing the function as an input
+# we are just passing the function name not with the '()'
+# Because with '()' it will trigger the function immediately
+# Higher Order Function: A function that takes another function as input and work with them
+screen.onkey(move_forwards, 'w')
+screen.onkey(move_backwards, 's')
+screen.onkey(turn_left, 'a')
+screen.onkey(turn_right, 'd')
+screen.onkey(clear, 'c')
+
+screen.exitonclick()
